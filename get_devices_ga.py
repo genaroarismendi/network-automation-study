e0e0fc71fe38e3cd811e1baa9633051d0ffc5653
g <genaroarismendi@icloud.com>
2025-02-16 18:22:50 +0000

    retomando practica de git el 16 de febrero 2025

--- get_devices_ga.py
+++ get_devices_ga.py
@@ -20,7 +20,7 @@ def get_devices():
     api_path = "https://sandboxdnac.cisco.com/dna"
     auth = ("devnetuser", "Cisco123!")
     headers = {"Content-Type": "application/json",
-        "X-Auth-Token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MGVjNGU0ZjRjYTdmOTIyMmM4MmRhNjYiLCJhdXRoU291cmNlIjoiaW50ZXJuYWwiLCJ0ZW5hbnROYW1lIjoiVE5UMCIsInJvbGVzIjpbIjVlOGU4OTZlNGQ0YWRkMDBjYTJiNjQ4ZSJdLCJ0ZW5hbnRJZCI6IjVlOGU4OTZlNGQ0YWRkMDBjYTJiNjQ4NyIsImV4cCI6MTcwOTkzNzQwOCwiaWF0IjoxNzA5OTMzODA4LCJqdGkiOiIzODk0ZThlZS04YTg2LTQ2N2EtYTVjMy03ZDc3YzdiYWJkZWQiLCJ1c2VybmFtZSI6ImRldm5ldHVzZXIifQ.SQs9sJGZ_gkz2w6RVjpxG8g3bfoNEC99pnfSS-SPmRtVhRiGIBKVIOHf-62eWg1Xm-rzYG12N0CNDp3HFO6KR3GsK5amJyCY9yzllBlFFD2gofyHBOIPmleXiXpD-xWrEBuATZ9i1Twd7IdWiXhNcRDdEUQUdFDj5rNak-Zjat3ElZqLa7IsR50npR2SNdwDv9mphUqxRKYAEUZ3akad5-v33xGPQE1Di_dvb3WshPQ2LumyZNWhXWKpGJOj_VYRH_TrQ5En7aizn76vgtV2h60O0z1AocS2q8_Hs64FyL6QLmkIl3kOuIPFuoz59lkvCUaSFDDbiYggQhvBp1MovA"}
+        "X-Auth-Token": "token_here"}
 
     # Issue HTTP POST request to the proper URL to request a token
     auth_resp = requests.get(
